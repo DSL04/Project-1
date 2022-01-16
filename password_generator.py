bbbcae2e7abdc62baa129610e8088be7de47f7b2
Abd-Ennour <Souit02@outlook.com>
2022-01-15 22:03:26 -0500

    Some of the skeleton code for my password generator along with the conditions my code is based of

--- password_generator.py
+++ password_generator.py
@@ -28,11 +28,32 @@ name=input("What is your name?")
 
 ascii_name_plaque("Alright " + name + ", let us try and create a password. Remember, you must have at least one of each type of character with a capital letter.")
 
-password=input("Now, please enter your password")
+password=input(str(("Now, please enter your password betweem 9-25 characters.")))
+
+    '''
+flag = True
+while flag:
+count = 0
+    '''
+    if len(password) < 9:
+        print("Sorry, your password is too short. Please try in between 9 and 25 characters.")
+        
+    elif len(password) > 25:
+        print("Sorry, your password is too lengthy. Please try in between 9 and 25 characters.")
+            
+    elif (password == "" ) or (password == "" ) or (password == "" ):
+        print("Sorry your password is too WEAK! Please try again:) ")
+
+    elif ("" in password) and ("" in password) and ("" in password):
+        print("Not bad, your password is decent. Not good enough though, Try again.")
+
+    elif ("" in password) and ("" in password) and ("" in password):
+        print("Noice, your password is strong! Now you have an ironclad password to protect you from Hackers.")
 
 #conditions:
-#- If all the characters in the password are simply either letters, numbers,or only one type of character, that password is deemed weak
-#- If all
+# The password must be from 9-25 characters in length
+#- If all the characters in the password are simply either letters, numbers,or only one type of a character, that password is deemed weak
+#- If the password
 #
 #
 #
@@ -40,6 +61,6 @@ password=input("Now, please enter your password")
 #
 #
 #
-#
-#
-#
+#- If the password has a consistent combination of letters(at least 4), numbers(at least 4) and special characters(at least 2), then the password is deemed Strong
+# Have special characters replaced for certain letters for example, the letter "a" can be "@" or the letter "o" can be "0"
+# After 10 tries, the UI will suggest a new password through a series of questions in which it will createa new password based off the questions
